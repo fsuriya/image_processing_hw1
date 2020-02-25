28c6fb898e978f9135dac7862ec139ae9d88c52b
Thanatossan <kotokikung@gmail.com>
2020-02-25 12:08:39 +0700

    bug write

--- 2. Point Operations.py
+++ 2. Point Operations.py
@@ -1,4 +1,5 @@
 from readpgm import read_pgm, list_to_2D_list, copy
+from writepgm import writepgm
 from etc_function import createHistogram
 # filename = "./image/2./Cameraman.pgm"
 filename = "./image/2./Cameraman.pgm"
@@ -14,7 +15,7 @@ histogram_old = createHistogram(converted_img)
 current_D = []
 miss_frequency = []
 miss_update = {}
-for i in range(255):
+for i in range(255):  # find miss colour
     if i not in histogram_old:
         miss_frequency.append(i)
 # complete with colore frequenct = 0
@@ -23,10 +24,11 @@ for key in miss_update:
     histogram_old.update({key: miss_update[key]})
 
 histogram_old = {key: histogram_old[key]
-                 for key in sorted(histogram_old.keys())}  # sorted  to dict
-print(histogram_old)
+                 for key in sorted(histogram_old.keys())}  # sorted to dict
+# print(histogram_old)
 histogram_db = histogram_old.copy()
 area = col*row
+
 for D in histogram_db:
     histogram_db[D] = histogram_db[D] / area  # H(D) / area
 Pcxy = 0
@@ -37,8 +39,22 @@ for D in histogram_db:
     histogram_db[D] = histogram_db[D]*255  # convert to CMF
 for D in histogram_db:
     histogram_db[D] = int(histogram_db[D] // 1)  # floor number
-print(histogram_db)
-# print(histogram_old)
-# print("------------------")
+
+histogram_db = dict((y, x) for x, y in histogram_db.items())
 # print(histogram_db)
-# histogram_new = {}
+print("-------------------")
+# for D in histogram_db:
+#     for K in histogram_old:
+#         if histogram_db[D] == K:
+#             histogram_db[D] = histogram_old[K]
+# Buggggg
+
+# current dict is <DB : DA >
+for i in range(len(mattrix_img)):
+    for j in range(len(mattrix_img[i])):
+        for key in histogram_db:
+            if mattrix_img[i][j] == histogram_db[key]:
+                mattrix_img[i][j] = key
+# print(mattrix_img)
+
+writepgm(filename, mattrix_img, col, row)
--- writepgm.py
+++ writepgm.py
@@ -1,1 +1,15 @@
 # Must be done someday
+def writepgm(filename, mattriximg, col, row):
+    filename = "test.pgm"
+    string = ""
+    for i in range(len(mattriximg)):
+        for j in range(len(mattriximg[i])):
+            mattriximg[i][j] = chr(mattriximg[i][j])
+            string += mattriximg[i][j]
+    print(len(mattriximg))
+    f = open(filename, "a")
+    f.write("P5 \r")
+    f.write(str(col) + " " + str(row) + "\r")
+    f.write("255 \r")
+    f.write(string)
+    f.close
